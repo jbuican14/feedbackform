e1ee41054fc8c89c166be69b23d6b4c2fc53e695
Juti Buican <juti.buican@bottomline.com>
2022-07-25 18:02:02 +0100

    Finishing up useContext (update)

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -63,9 +63,18 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const res = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": 'application/json',
+      },
+      body: JSON.stringify(updItem)
+    });
+    const data = await res.json();
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
